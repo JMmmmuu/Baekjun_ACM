5c3f0148317df0f89b14e5dde34487cdd31ef4a9
Yuseok <mkyseok1550@gmail.com>
2018-10-11 12:15:40 +0900

    Complete 16172

--- 16172.c
+++ 16172.c
@@ -14,17 +14,17 @@ int main()
     char* newIn = (char*)malloc(1000 * sizeof(char));
     int i = 0, cnt = 0, flag = 0;
 
-    while (input[i] != '\n') {
+    //while (input[i] != '\n') {
+    for (i=0; i<strlen(input); i++) {
         if(input[i] < '0' || input[i] > '9') {
             newIn[cnt] = input[i];
             cnt++;
         }
-        i++;
     }
-    newIn[cnt] = '\n';
+    //newIn[cnt] = '\n';
 
     for (i = 0; i < strlen(newIn) - strlen(key) + 1; i++) {
-        if (strncmp(newIn+i, key, strlen(key))) {
+        if (strncmp(newIn+i, key, strlen(key)) == 0) {
             flag++;
             break;
         }
@@ -33,7 +33,7 @@ int main()
     else printf("0\n");
 
 
-    printf("%s%s%s", input, key, newIn);
+    //printf("%s%s%s", input, key, newIn);
 
     return 0;
 }
