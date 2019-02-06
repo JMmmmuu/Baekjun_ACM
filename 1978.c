e6be8830d71d9fa10b52b1880bab8c78dce05a0e
JMmmmuu <mkyseok1550@gmail.com>
2019-02-06 18:04:08 +0900

    1978 - find prime number

--- 1978.c
+++ 1978.c
@@ -1,2 +1,35 @@
+/*************************************************
+ ****************** find prime *******************
+ ******************** Yuseok *********************
+ ******************** 190206 *********************
+ *************************************************/
 #include <stdio.h>
+#include <stdlib.h>
 
+int isPrime(int veri);
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int veri;
+    int cnt = 0;
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &veri);
+        if (isPrime(veri)) cnt++;
+    }
+
+    printf("%d\n", cnt);
+
+    return 0;
+}
+
+int isPrime(int veri) {
+    /* O(n) algorithm */
+    if (veri <= 1) return 0;
+    for (int i = 2; i < veri; i++) {
+        if (veri % i == 0) return 0;
+    }
+    return 1;
+}
